da6d33cf861f86b6125e720527b761eaf27c4d6e
Shrut <shrutpatel20@gmail.com>
2024-05-22 16:01:54 -0500

    add trash functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const background = document.querySelector("body");
 const mainContainer = document.querySelector(".main");
 const bookContainer = document.querySelector(".book-container");
-const myLibrary = [];
+let myLibrary = [];
 
 //-------------------------------book objects-------------------------
 function Book(title, author, coverUrl, readStatus, genre) {
@@ -46,7 +46,7 @@ const showDialogBtn = document.getElementById("add-book");
 const closeDialogBtn = document.getElementById("closeDialogBtn");
 
 showDialogBtn.addEventListener("click", () => {
-   form.reset();
+  form.reset();
   dialog.showModal();
 });
 
@@ -73,31 +73,98 @@ form.addEventListener("submit", (event) => {
 });
 
 function updateDisplay() {
-    clearDisplay();
+  clearDisplay();
   myLibrary.forEach((book) => {
     const bookDiv = document.createElement("div");
+
+    let buttonColor = "rgb(120, 201, 130)";
+
+    if (book.readStatus === "Read" || book.readStatus === "All") {
+      buttonColor = "rgb(120, 201, 130)";
+    } else {
+      buttonColor = "rgb(201, 120, 139)";
+    }
+
     bookDiv.classList.add("book");
     bookDiv.innerHTML = `
         <div class="book-cover" style="background-image: url(${book.coverUrl});">
         </div>
         <div class="book-info">
-          <h2>${book.title}</h2>
-          <div class="book-min-info">
-            <p>${book.author}</p>
-            <p>${book.genre}</p>
+          <h2 class="title">${book.title}</h2>
+          <div class="main-info">
+            <div class="book-min-info">
+              <p>${book.author}</p>
+              <p>${book.genre}</p>
+            </div>
+            <div class="book-icon">
+              <img src="imgs/pencil.svg" alt="">
+              <svg xmlns="http://www.w3.org/2000/svg" id="book-trash" viewBox="0 0 24 24" width="30px" height="30px" fill="white"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>
+            </div>
           </div>
-          <button>${book.readStatus}</button>
+          <button id="reading-status" style="background-color: ${buttonColor};">${book.readStatus}</button>
         </div>
     `;
-    
+
     bookContainer.appendChild(bookDiv);
   });
+
+  addReadingListeners();
+  addBookListeners();
 }
 
 function clearDisplay() {
-    const books = document.querySelectorAll(".book");
-    books.forEach((book) => {
-        bookContainer.removeChild(book);
-    });
+  const books = document.querySelectorAll(".book");
+  books.forEach((book) => {
+    bookContainer.removeChild(book);
+  });
 }
 //-----------------------------add book ----------------------------------
+
+//----------------------------change-read-status--------------------------//
+
+function addReadingListeners() {
+  const readBtns = document.querySelectorAll("#reading-status");
+
+  readBtns.forEach((button) => {
+    button.addEventListener("click", () => {
+      const bookContainer = button.closest(".book");
+      const bookTitle = bookContainer.querySelector(".title");
+      updateBook(bookTitle.textContent);
+    });
+  });
+}
+
+function updateBook(title) {
+  myLibrary.forEach((book) => {
+    if (book.title === title) {
+      if (book.readStatus === "Read") {
+        book.readStatus = "Unread";
+      } else {
+        book.readStatus = "Read";
+      }
+    }
+  });
+  updateDisplay();
+}
+
+//----------------------------change-read-status--------------------------//
+
+//----------------------------trash a book card---------------------------//
+
+function addBookListeners() {
+  const trashes = document.querySelectorAll("#book-trash");
+
+  trashes.forEach((trash) => {
+    trash.addEventListener("click", () => {
+      const bookContainer = trash.closest(".book");
+      const bookTitle = bookContainer.querySelector(".title");
+      const newLibrary = myLibrary.filter(
+        (book) => book.title !== bookTitle.textContent
+      );
+      myLibrary = newLibrary;
+      updateDisplay();
+    });
+  });
+}
+
+//----------------------------trash a book card---------------------------//
